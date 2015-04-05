4c310b3c926c15b14e475ce4ff9aaf19d08e6bd6
Denis Papathanasiou <denis@papathanasiou.org>
2015-04-05 19:02:18 -0400

    Did the uncurry exercise

--- 2-Getting-Started/Curry.scala
+++ 2-Getting-Started/Curry.scala
@@ -4,6 +4,10 @@ object Curry {
     (a: A) => (b: B) => f(a, b)
   }
 
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C = {
+    (a: A, b: B) => f(a)(b) 
+  }
+
   def main(args: Array[String]): Unit = {
     // curry a simple sum function
     val myPlus = curry((a: Int, b: Int) => a+b)
@@ -11,5 +15,9 @@ object Curry {
     val plus6  = myPlus(6)
     // take any int input and add 6
     println("Curried: args(0) plus 6 = %d".format(plus6(args(0).toInt)))
+
+    // and now uncurry the prior curried fn
+    val myRegularPlus = uncurry(myPlus)
+    println("Uncurried: args(0) plus args(1) = %d".format(myRegularPlus(args(0).toInt, args(1).toInt)))
   }
 }
